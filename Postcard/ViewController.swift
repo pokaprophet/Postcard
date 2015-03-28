4333d141c25d595454133484f4e186f890c39cad
IAN WOODWARD <iwoodster@IAN-WOODWARDs-Mac-mini.local>
2015-03-28 22:42:05 +0000

    added a comment to test commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //Adding a comment to test commit
+        
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {messageLabel.hidden = false
